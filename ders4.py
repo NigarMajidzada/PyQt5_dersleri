92c555b592857d617af75ca9b77c9b98ea5aff5b
NigarMajidzada <107548352+NigarMajidzada@users.noreply.github.com>
2023-12-27 20:33:54 +0400

    Initial commit

--- ders4.py
+++ ders4.py
@@ -7,15 +7,33 @@ def  Pencere():
     okay=QtWidgets.QPushButton("Tamam")
     cancel=QtWidgets.QPushButton("Iptal")
 
-    h_box=QtWidgets.QHBoxLayout()
+    # h_box=QtWidgets.QHBoxLayout()
+    # h_box.addWidget(okay)
+    # h_box.addStretch()
+    # h_box.addWidget(cancel)
 
+    # v_box=QtWidgets.QVBoxLayout()
+    #
+    # v_box.addWidget(okay)
+    # v_box.addStretch()
+    # v_box.addWidget(cancel)
+
+
+
+    h_box=QtWidgets.QHBoxLayout()
+    h_box.addStretch()
     h_box.addWidget(okay)
     h_box.addWidget(cancel)
-    h_box.addStretch()
+
+    v_box=QtWidgets.QVBoxLayout()
+    v_box.addStretch()
+    v_box.addLayout(h_box)
+
+
     pencere=QtWidgets.QWidget()
     pencere.setWindowTitle("PyQT5  Ders4")
 
-    pencere.setLayout(h_box)
+    pencere.setLayout(v_box)
 
     pencere.setGeometry(100,100,500,500)
     pencere.show()
